8fa7c748982510b6ecb7c64e55c3088ad21ec9c8
IrateLizard20 <iratelizard20@gmail.com>
2024-06-06 22:40:26 -0400

    art gallery update + BACK BUTTONS
    added art to mobile art gallery sections as well as adding a new section
    
    I ADDED BACK BUTTONS BABYYYYYYYY LETS GOOOOOOO

---
+++ image_gallery/mobile/mobile.js
@@ -0,0 +1,3 @@
+function Back(){
+    window.open("/website/image_gallery/mobile/mobile_art_gallery.html","_self")
+}
---
+++ image_gallery/pc/pc.js
@@ -0,0 +1,3 @@
+function Back(){
+    window.open("/website/image_gallery/pc/pc_art_gallery.html","_self")
+}
